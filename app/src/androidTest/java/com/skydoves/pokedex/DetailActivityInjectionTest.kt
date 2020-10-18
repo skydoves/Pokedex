3f7f36153c4d4592ce47524ee85db3baa6aaa153
yehezkiel <yehezkiel@tokopedia.com>
2020-10-18 10:13:54 +0700

    remove unused import

--- app/src/androidTest/java/com/skydoves/pokedex/DetailActivityInjectionTest.kt
+++ app/src/androidTest/java/com/skydoves/pokedex/DetailActivityInjectionTest.kt
@@ -18,7 +18,6 @@ package com.skydoves.pokedex
 
 import android.content.Intent
 import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.Observer
 import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
