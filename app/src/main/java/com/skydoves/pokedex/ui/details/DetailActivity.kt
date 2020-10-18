a7be3e877764a5c899e2a16f22a0725b4b4c3745
skydoves <skydoves@naver.com>
2020-10-18 13:39:05 +0900

    Refactor DetailActivity using extensions

--- app/src/main/java/com/skydoves/pokedex/ui/details/DetailActivity.kt
+++ app/src/main/java/com/skydoves/pokedex/ui/details/DetailActivity.kt
@@ -24,6 +24,7 @@ import androidx.annotation.VisibleForTesting
 import com.skydoves.pokedex.R
 import com.skydoves.pokedex.base.DataBindingActivity
 import com.skydoves.pokedex.databinding.ActivityDetailBinding
+import com.skydoves.pokedex.extensions.argument
 import com.skydoves.pokedex.extensions.onTransformationEndContainerApplyParams
 import com.skydoves.pokedex.model.Pokemon
 import com.skydoves.transformationlayout.TransformationCompat
@@ -39,29 +40,25 @@ class DetailActivity : DataBindingActivity() {
 
   @VisibleForTesting
   val viewModel: DetailViewModel by viewModels {
-    DetailViewModel.provideFactory(
-      detailViewModelFactory,
-      pokemonItem.name
-    )
+    DetailViewModel.provideFactory(detailViewModelFactory, pokemonItem.name)
   }
 
   private val binding: ActivityDetailBinding by binding(R.layout.activity_detail)
-
-  private val pokemonItem: Pokemon
-    get() = requireNotNull(intent.getParcelableExtra(EXTRA_POKEMON))
+  private val pokemonItem: Pokemon by argument(EXTRA_POKEMON)
 
   override fun onCreate(savedInstanceState: Bundle?) {
     onTransformationEndContainerApplyParams()
     super.onCreate(savedInstanceState)
     binding.apply {
-      pokemon = pokemonItem
       lifecycleOwner = this@DetailActivity
+      pokemon = pokemonItem
       vm = viewModel
     }
   }
 
   companion object {
-    @VisibleForTesting const val EXTRA_POKEMON = "EXTRA_POKEMON"
+    @VisibleForTesting
+    const val EXTRA_POKEMON = "EXTRA_POKEMON"
 
     fun startActivity(transformationLayout: TransformationLayout, pokemon: Pokemon) {
       val context = transformationLayout.context
