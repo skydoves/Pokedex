bdbe1d8649bd2a0a64536ee29813ca4bab607eb2
skydoves <skydoves@naver.com>
2022-05-31 20:50:35 +0900

    Remove the experimental annotation for the MainCoroutinesRule

--- app/src/test/java/com/skydoves/pokedex/MainCoroutinesRule.kt
+++ app/src/test/java/com/skydoves/pokedex/MainCoroutinesRule.kt
@@ -19,7 +19,6 @@
 package com.skydoves.pokedex
 
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.TestDispatcher
 import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlinx.coroutines.test.resetMain
@@ -27,16 +26,15 @@ import kotlinx.coroutines.test.setMain
 import org.junit.rules.TestWatcher
 import org.junit.runner.Description
 
-@ExperimentalCoroutinesApi
 class MainCoroutinesRule(
   val testDispatcher: TestDispatcher = UnconfinedTestDispatcher()
 ) : TestWatcher() {
 
-  override fun starting(description: Description?) {
+  override fun starting(description: Description) {
     Dispatchers.setMain(testDispatcher)
   }
 
-  override fun finished(description: Description?) {
+  override fun finished(description: Description) {
     super.finished(description)
     Dispatchers.resetMain()
   }
