ec67e2b4ae13c4a6435194830e8a41a04536899c
skydoves <skydoves@naver.com>
2022-08-15 14:51:34 +0900

    Refactor spotless plugin and add a license for other formats

--- app/src/main/java/com/skydoves/pokedex/model/Pokemon.kt
+++ app/src/main/java/com/skydoves/pokedex/model/Pokemon.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+ 
 package com.skydoves.pokedex.model
 
 import android.os.Parcelable
@@ -28,7 +28,8 @@ import kotlinx.parcelize.Parcelize
 @JsonClass(generateAdapter = true)
 data class Pokemon(
   var page: Int = 0,
-  @field:Json(name = "name") @PrimaryKey val name: String,
+  @field:Json(name = "name") @PrimaryKey
+  val name: String,
   @field:Json(name = "url") val url: String
 ) : Parcelable {
 
