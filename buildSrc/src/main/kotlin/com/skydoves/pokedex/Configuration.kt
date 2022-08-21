0d586679aaebfeee1bce9b89f5d3d3dd3adfd644
skydoves <skydoves@naver.com>
2022-08-21 20:58:36 +0900

    Increment minor version of the version code

--- buildSrc/src/main/kotlin/com/skydoves/pokedex/Configuration.kt
+++ buildSrc/src/main/kotlin/com/skydoves/pokedex/Configuration.kt
@@ -5,8 +5,8 @@ object Configuration {
   const val targetSdk = 32
   const val minSdk = 23
   const val majorVersion = 1
-  const val minorVersion = 0
-  const val patchVersion = 7
+  const val minorVersion = 1
+  const val patchVersion = 0
   const val versionName = "$majorVersion.$minorVersion.$patchVersion"
   const val versionCode = 8
 }
