25b33d2e06ca5d3729c45d1db1bd41e01ca1a5b8
skydoves <skydoves@naver.com>
2023-01-12 09:18:48 +0900

    Update AGP to 7.3.1 and Kotlin to 1.8.0

--- buildSrc/src/main/kotlin/com/skydoves/pokedex/Configuration.kt
+++ buildSrc/src/main/kotlin/com/skydoves/pokedex/Configuration.kt
@@ -1,8 +1,8 @@
 package com.skydoves.pokedex
 
 object Configuration {
-  const val compileSdk = 32
-  const val targetSdk = 32
+  const val compileSdk = 33
+  const val targetSdk = 33
   const val minSdk = 23
   const val majorVersion = 1
   const val minorVersion = 1
