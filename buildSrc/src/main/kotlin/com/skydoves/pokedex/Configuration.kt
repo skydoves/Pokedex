1b3e0a12562a06519ab9627e64c8ea096647f911
skydoves <skydoves@naver.com>
2023-04-01 16:22:56 +0900

    Increment a patch version

--- buildSrc/src/main/kotlin/com/skydoves/pokedex/Configuration.kt
+++ buildSrc/src/main/kotlin/com/skydoves/pokedex/Configuration.kt
@@ -6,7 +6,7 @@ object Configuration {
   const val minSdk = 23
   const val majorVersion = 1
   const val minorVersion = 1
-  const val patchVersion = 0
+  const val patchVersion = 1
   const val versionName = "$majorVersion.$minorVersion.$patchVersion"
-  const val versionCode = 8
+  const val versionCode = 9
 }
