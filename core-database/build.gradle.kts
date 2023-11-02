e145f154ecd89c0a9d804a9a67bdb61950d04835
skydoves <skydoves@naver.com>
2023-11-02 14:30:44 +0900

    Add lint abort errors

--- core-database/build.gradle.kts
+++ core-database/build.gradle.kts
@@ -44,6 +44,10 @@ android {
     sourceCompatibility = JavaVersion.VERSION_17
     targetCompatibility = JavaVersion.VERSION_17
   }
+
+  lint {
+    abortOnError = false
+  }
 }
 
 dependencies {
--- core-test/build.gradle.kts
+++ core-test/build.gradle.kts
@@ -33,6 +33,10 @@ android {
     sourceCompatibility = JavaVersion.VERSION_17
     targetCompatibility = JavaVersion.VERSION_17
   }
+
+  lint {
+    abortOnError = false
+  }
 }
 
 dependencies {
