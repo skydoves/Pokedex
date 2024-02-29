f31d8efd47d6deb4786a91fffd354ef4954cd1f2
YOUNG HO CHA <ganachoco@linecorp.com>
2024-03-01 01:28:23 +0900

    Applied maven content filtering

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -18,19 +18,80 @@ enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
 pluginManagement {
   repositories {
-    google()
-    mavenCentral()
+    // fetch plugins from google maven (https://maven.google.com)
+    google() {
+      content {
+        includeGroupByRegex("androidx\\..*")
+        includeGroupByRegex("com\\.android(\\..*|)")
+        includeGroupByRegex("com\\.google\\.android\\..*")
+        includeGroupByRegex("com\\.google\\.firebase(\\..*|)")
+        includeGroupByRegex("com\\.google\\.gms(\\..*|)")
+        includeGroupByRegex("com\\.google\\.mlkit")
+        includeGroupByRegex("com\\.google\\.oboe")
+        includeGroupByRegex("com\\.google\\.prefab")
+        includeGroupByRegex("com\\.google\\.testing\\.platform")
+      }
+      mavenContent {
+        releasesOnly()
+      }
+    }
+
+    // fetch dagger plugin only
+    mavenCentral() {
+      content {
+        includeGroup("com.google.dagger")
+        includeGroup("com.google.dagger.hilt.android")
+      }
+      mavenContent {
+        releasesOnly()
+      }
+    }
+
+    // fetch plugins from gradle plugin portal (https://plugins.gradle.org)
     gradlePluginPortal()
-    maven(url = "https://oss.sonatype.org/content/repositories/snapshots/")
+
+    // fetch snapshot plugins from sonatype
+    maven(url = "https://oss.sonatype.org/content/repositories/snapshots/") {
+      mavenContent {
+        snapshotsOnly()
+      }
+    }
   }
 }
 dependencyResolutionManagement {
   repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
   repositories {
-    google()
-    mavenCentral()
-    gradlePluginPortal()
-    maven(url = "https://oss.sonatype.org/content/repositories/snapshots/")
+    // fetch libraries from google maven (https://maven.google.com)
+    google() {
+      content {
+        includeGroupByRegex("androidx\\..*")
+        includeGroupByRegex("com\\.android(\\..*|)")
+        includeGroupByRegex("com\\.google\\.android\\..*")
+        includeGroupByRegex("com\\.google\\.firebase(\\..*|)")
+        includeGroupByRegex("com\\.google\\.gms(\\..*|)")
+        includeGroupByRegex("com\\.google\\.mlkit")
+        includeGroupByRegex("com\\.google\\.oboe")
+        includeGroupByRegex("com\\.google\\.prefab")
+        includeGroupByRegex("com\\.google\\.testing\\.platform")
+      }
+      mavenContent {
+        releasesOnly()
+      }
+    }
+
+    // fetch libraries from maven central
+    mavenCentral() {
+      mavenContent {
+        releasesOnly()
+      }
+    }
+
+    // fetch snapshot libraries from sonatype
+    maven(url = "https://oss.sonatype.org/content/repositories/snapshots/") {
+      mavenContent {
+        snapshotsOnly()
+      }
+    }
   }
 }
 rootProject.name = "Pokedex"
