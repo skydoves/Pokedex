61d94731701101a170a1125fbe5b04a349cbc2c7
skydoves <skydoves@naver.com>
2025-03-19 20:25:56 +0900

    Prepare for release 1.1.3

--- buildSrc/src/main/kotlin/com/skydoves/pokedex/Configuration.kt
+++ buildSrc/src/main/kotlin/com/skydoves/pokedex/Configuration.kt
@@ -3,7 +3,7 @@ package com.skydoves.pokedex
 object Configuration {
   const val majorVersion = 1
   const val minorVersion = 1
-  const val patchVersion = 2
+  const val patchVersion = 3
   const val versionName = "$majorVersion.$minorVersion.$patchVersion"
   const val versionCode = 10
 }
